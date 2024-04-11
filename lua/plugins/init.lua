01c2384a5b234213212ea56fde2b23277711f127
Robert Brandin <me@robertbrandin.com>
2024-04-10 22:51:22 -0600

    Updates

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -39,5 +39,26 @@ return {
       },
     },
   },
-
+  {
+    "jamestthompson3/nvim-remote-containers",
+    lazy = false,
+  },
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup {
+        api_key_cmd = "op read op://pfq2sghne45z45g7au5klua5q4/OpenAI/credential --no-newline",
+        openai_params = {
+          model = "gpt-4-turbo",
+        },
+      }
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+  },
 }
